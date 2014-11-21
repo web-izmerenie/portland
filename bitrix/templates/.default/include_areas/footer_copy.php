0a4bbd51198d5e89ba226a107948c617c92542f0
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2014-11-21 16:24:32 +0300

    remove info about photographer in footer

--- bitrix/templates/.default/include_areas/footer_copy.php
+++ bitrix/templates/.default/include_areas/footer_copy.php
@@ -2,4 +2,4 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
 &copy; &laquo;Портлэнд&raquo; 2014 <span class="nowrap">Все права защищены</span><br />
-<div class="innafoto"><span class="camera"></span> Фото на сайте &mdash; <a href="https://vk.com/public_innafoto" class="custom-link custom-link--orange" target="_blank"><span class="custom-link-name">Инна Ковальская</span></a></div>
+<!--<div class="innafoto"><span class="camera"></span> Фото на сайте &mdash; <a href="https://vk.com/public_innafoto" class="custom-link custom-link--orange" target="_blank"><span class="custom-link-name">Инна Ковальская</span></a></div>-->
